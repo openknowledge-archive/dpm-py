0df909f414e824d322f66abb33ae6704e0e47bb1
Rajiv <kittdotech@gmail.com>
2016-11-16 19:46:57 +0530

    [publish][m #32]: Added Test for Multiple README
    Added Test for Multiple Readme

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -86,3 +86,40 @@ class ReadmeTest(BaseCliTestCase):
 
         # Checking README
         self.assertIn('Publishing Package without README', result.output)
+
+    @patch('dpm.client.do_publish.filter', lambda a, b: ['README.', 'README.txt',
+                                                         'README.md', 'README'])
+    @patch('dpm.client.do_publish.open', mock_open())  # mock csv file open
+    @patch('dpm.client.do_publish.getsize', lambda a: 5)  # mock csv file size
+    def test_readme_sucess_for_multiple_readme(self):
+        # GIVEN the registry server that accepts any user
+        responses.add(
+            responses.POST, 'https://example.com/api/auth/token',
+            json={'token': 'blabla'},
+            status=200)
+        # AND registry server accepts any datapackage
+        responses.add(
+            responses.PUT, 'https://example.com/api/package/user/some-datapackage',
+            json={'message': 'OK'},
+            status=200)
+        # AND registry server gives bitstore upload url
+        responses.add(
+            responses.POST, 'https://example.com/api/auth/bitstore_upload',
+            json={'key': 'https://s3.fake/put_here'},
+            status=200)
+        # AND s3 server allows data upload
+        responses.add(
+            responses.PUT, 'https://s3.fake/put_here',
+            json={'message': 'OK'},
+            status=200)
+        # AND registry server successfully finalizes upload
+        responses.add(
+            responses.GET, 'https://example.com/api/package/user/some-datapackage/finalize',
+            json={'message': 'OK'},
+            status=200)
+        # WHEN `dpm publish` is invoked
+        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
+
+        # Checking README uploading first match
+        self.assertNotIn('Publishing Package without README', result.output)
+        self.assertIn('Uploading README.txt', result.output)
