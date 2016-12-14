a45a96d98f26a5f7820608beb9bb1a16fa46a6ea
Roman Evstifeev <someuniquename@gmail.com>
2016-12-14 21:02:06 +0300

    Add client configuration tests.
    Addreses #43

--- dpm/client/__init__.py
+++ dpm/client/__init__.py
@@ -9,20 +9,29 @@ import datapackage
 class DpmException(Exception):
     pass
 
+
+class ConfigError(Exception):
+    """ The configuration passed to the client is malformed """
+    pass
+
+
 class Client(object):
 
-    def __init__(self, data_package_path='', config_path="~/.dpm/config", click=None):
+    def __init__(self, config, data_package_path='', click=None):
         if not data_package_path:
             data_package_path = os.getcwd()
         data_package_path = os.path.abspath(data_package_path)
         # may want to use the datapackage-py here
         self.datapackage = self._load_dp(data_package_path)
 
-        # load config ... 
-        self.config = self._load_config(config_path)
+        try:
+            self.server = config['server']
+            self.username = config['username']
+            self.password = config['password']
+        except KeyError as e:
+            raise ConfigError('Configuration error: %s is required' % str(e))
 
         self.click = click
-
 
     def _load_dp(self, path):
         dppath = os.path.join(path, 'datapackage.json')
@@ -33,11 +42,6 @@ class Client(object):
 
         dp = datapackage.DataPackage(dppath)
         return dp
-
-
-    def _load_config(self, path):
-        pass
-
 
     def validate(self):
         self.datapackage.validate()
--- tests/test_client.py
+++ tests/test_client.py
@@ -2,27 +2,71 @@ import unittest
 import pytest
 import os
 
-from dpm.client import Client, DpmException
+from dpm.client import Client, DpmException, ConfigError
 
 dp1_path = 'tests/fixtures/dp1'
 
 class TestClient(unittest.TestCase):
+    config = {
+        'username': 'user',
+        'password': 'password',
+        'server': 'http://127.0.0.1:5000'
+    }
+
     def test___init__fails_no_data_package(self):
         with pytest.raises(DpmException):
-            client = Client()
+            client = Client(self.config)
         try:
-            client = Client()
+            client = Client(self.config)
         except DpmException as e:
             assert e.message.startswith('No Data Package found at %s' %
                     os.getcwd())
     
     def test___init__datapackage_ok(self):
-        client = Client(dp1_path)
+        client = Client(self.config, dp1_path)
         assert client.datapackage
         assert client.datapackage.base_path.endswith(dp1_path)
 
-    def test__init__config_ok(self):
-        pass
-    
+    def test__init__config_password_missing(self):
+        """
+        When the 'password' is missing in the config, the client should raise ConfigError.
+        """
+        config = {'username': 'user', 'server': 'server'}
+        with pytest.raises(ConfigError):
+            client = Client(config, dp1_path)
+
+        try:
+            client = Client(config, dp1_path)
+        except ConfigError as e:
+            assert "'password' is required" in e.message
+
+    def test__init__config_username_missing(self):
+        """
+        When the 'username' is missing in the config, the client should raise ConfigError.
+        """
+        config = {'password': 'pwd', 'server': 'server'}
+        with pytest.raises(ConfigError):
+            client = Client(config, dp1_path)
+
+        try:
+            client = Client(config, dp1_path)
+        except ConfigError as e:
+            assert "'username' is required" in e.message
+
+    def test__init__config_server_missing(self):
+        """
+        When the 'server' is missing in the config, the client should raise ConfigError.
+        """
+        config = {'username': 'user', 'password': 'pwd'}
+
+        with pytest.raises(ConfigError):
+            client = Client(config, dp1_path)
+
+        try:
+            client = Client(config, dp1_path)
+        except ConfigError as e:
+            assert "'server' is required" in e.message
+
+
     # TODO: test validate
 
