ab5a579b94945efcc02a1ef7981e39cf99392b19
Roman Evstifeev <someuniquename@gmail.com>
2016-12-15 18:11:12 +0300

    Add Client._apirequest() with tests
    Addreses #43

--- dpm/client/__init__.py
+++ dpm/client/__init__.py
@@ -4,14 +4,41 @@ from .do_publish import publish
 from .do_validate import validate
 
 import os
+
 import datapackage
+import requests
+import six
+
 
 class DpmException(Exception):
     pass
 
+class NetworkError(Exception):
+    """ There was a connection error during request. """
+    pass
+
+class AuthError(Exception):
+    """ Recieved malformed response form authentication server. """
+    pass
 
 class ConfigError(Exception):
-    """ The configuration passed to the client is malformed """
+    """ The configuration passed to the client is malformed. """
+    pass
+
+class JSONDecodeError(Exception):
+    """ Failed to decode responce JSON. """
+    def __init__(self, request, message):
+        self.request = request
+        self.message = message
+
+class HTTPStatusError(Exception):
+    """ Response status_code indicated error processing the request. """
+    def __init__(self, request, message):
+        self.request = request
+        self.message = message
+
+
+class MissingCredentialsError(Exception):
     pass
 
 
@@ -32,6 +59,7 @@ class Client(object):
             raise ConfigError('Configuration error: %s is required' % str(e))
 
         self.click = click
+        self.token = None
 
     def _load_dp(self, path):
         dppath = os.path.join(path, 'datapackage.json')
@@ -49,9 +77,67 @@ class Client(object):
         # should we really check this here? Good question i think ...
         for idx, resource in enumerate(self.datapackage.resources):
             if not exists(resource.local_data_path):
-                raise DpmException('Resource at index %s with name % and path %s does not exist on disk' % (
+                raise DpmException('Resource at index %s with name %s and path %s does not exist on disk' % (
                     idx, resource.name, resource.local_data_path)
                     )
 
         return True
+
+    def ensure_auth(self):
+        """
+        Get auth token from the server using credentials. Token can be used in future
+        requests to the server.
+
+        # TODO: is this private or useful outside?
+        # TODO: refresh expired token
+        """
+        if self.token:
+            return self.token
+
+        if not (self.username and self.password):
+            raise MissingCredentialsError('User credentials are missing.')
+
+        #echo('Authenticating ... ', nl=False)  # TODO: logging
+        authresponse = self._apirequest(
+                method='POST',
+                url='%s/api/auth/token' % self.server,
+                json={'username': self.username, 'secret': self.password})
+
+        self.token = authresponse.json().get('token')
+        if not self.token:
+            raise AuthError('Server did not return auth token')
+
+        return self.token
+
+    def _apirequest(self, method, *args, **kwargs):
+        """
+        General request-response processing routine for dpr-api server.
+
+        :return:
+            Response -- requests.Response instance
+
+        """
+        methods = {
+            'POST': requests.post,
+            'PUT': requests.put,
+            'GET': requests.get,
+            'DELETE': requests.delete
+        }
+
+        headers = kwargs.pop('headers', {})
+        if self.token:
+            headers.setdefault('Authorization', 'Bearer %s' % self.token)
+
+        response = methods.get(method)(*args, headers=headers, **kwargs)
+
+        try:
+            response.json()
+        except Exception as e:
+            six.raise_from(
+                JSONDecodeError(response, message='Failed to decode JSON response from server'), e)
+
+        if response.status_code not in (200, 201):
+            raise HTTPStatusError(response, message='Error %s' % response.status_code)
+
+        return response
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,18 +1,31 @@
 import unittest
-import pytest
 import os
 
-from dpm.client import Client, DpmException, ConfigError
+import pytest
+import requests
+import responses
+from mock import patch
+
+from dpm.client import Client, DpmException, ConfigError, NetworkError, JSONDecodeError, HTTPStatusError
+from .base import BaseTestCase
 
 dp1_path = 'tests/fixtures/dp1'
 
-class TestClient(unittest.TestCase):
+
+class BaseClientTestCase(BaseTestCase):
+    # Valid config for tests.
     config = {
         'username': 'user',
         'password': 'password',
         'server': 'http://127.0.0.1:5000'
     }
 
+
+class ClientInitTest(BaseClientTestCase):
+    """
+    `Client.__init__()` should raise exceptions if config is malformed or
+    datapackage is missing.
+    """
     def test___init__fails_no_data_package(self):
         with pytest.raises(DpmException):
             client = Client(self.config)
@@ -38,7 +51,7 @@ class TestClient(unittest.TestCase):
         try:
             client = Client(config, dp1_path)
         except ConfigError as e:
-            assert "'password' is required" in e.message
+            assert "'password' is required" in str(e)
 
     def test__init__config_username_missing(self):
         """
@@ -51,7 +64,7 @@ class TestClient(unittest.TestCase):
         try:
             client = Client(config, dp1_path)
         except ConfigError as e:
-            assert "'username' is required" in e.message
+            assert "'username' is required" in str(e)
 
     def test__init__config_server_missing(self):
         """
@@ -65,8 +78,71 @@ class TestClient(unittest.TestCase):
         try:
             client = Client(config, dp1_path)
         except ConfigError as e:
-            assert "'server' is required" in e.message
+            assert "'server' is required" in str(e)
 
+
+class ClientApirequestTest(BaseClientTestCase):
+    def setUp(self):
+        # GIVEN client instance with valid auth token
+        self.client = Client(self.config, dp1_path)
+        self.client.token = '123'
+
+    def test_connerror_oserror(self):
+        # GIVEN socket that throws OSError
+        with patch("socket.socket.connect", side_effect=OSError) as mocksock:
+            # WHEN client._apirequest is invoked
+            try:
+                result = self.client._apirequest(method='POST', url=self.client.server)
+            except Exception as e:
+                result = e
+
+            # THEN ConnectionError should be raised
+            assert isinstance(result, requests.ConnectionError)
+
+    def test_json_decode_error(self):
+        # GIVEN the server that responds with some invalid JSON
+        responses.add(
+            responses.POST, 'http://127.0.0.1:5000',
+            body="some invalid json",
+            status=200)
+
+        # WHEN client._apirequest is invoked
+        try:
+            result = self.client._apirequest(method='POST', url=self.client.server)
+        except Exception as e:
+            result = e
+
+        # THEN JSONDecodeError should be raised
+        assert isinstance(result, JSONDecodeError)
+
+    def test_http_status_error(self):
+        # GIVEN the server that responds with unsuccessful status_code (400)
+        responses.add(
+            responses.POST, 'http://127.0.0.1:5000',
+            json={'message': 'some error'},
+            status=400)
+
+        # WHEN client._apirequest is invoked
+        try:
+            result = self.client._apirequest(method='POST', url=self.client.server)
+        except Exception as e:
+            result = e
+
+        # THEN HTTPStatusError should be raised
+        assert isinstance(result, HTTPStatusError)
+
+    def test_success(self):
+        # GIVEN the server that responds with successful and valid response
+        responses.add(
+            responses.POST, 'http://127.0.0.1:5000',
+            json={'message': 'ok'},
+            status=200)
+
+        # WHEN client._apirequest is invoked
+        result = self.client._apirequest(method='POST', url=self.client.server)
+
+        # THEN result should be Response instance
+        assert isinstance(result, requests.Response)
 
     # TODO: test validate
 
