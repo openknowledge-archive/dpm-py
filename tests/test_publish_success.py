f1f9659ab9a3df90f71fb7daf8f6c1847d58617d
Roman Evstifeev <someuniquename@gmail.com>
2016-12-19 20:24:26 +0300

    Fix test_publish_success.py
    Addresses #55

--- tests/test_publish_success.py
+++ tests/test_publish_success.py
@@ -12,17 +12,7 @@ from mock import patch, mock_open
 from six import string_types
 
 from dpm.main import cli
-from .base import BaseCliTestCase
-
-
-def jsonify(data):
-    if not data:
-        return ''
-    if isinstance(data, bytes):
-        return json.loads(data.decode('utf8'))
-    if not isinstance(data, string_types):
-        return data.read(100)
-    return json.loads(data)
+from .base import BaseCliTestCase, StringIO, jsonify
 
 
 class PublishSuccessTest(BaseCliTestCase):
@@ -34,18 +24,19 @@ class PublishSuccessTest(BaseCliTestCase):
     def setUp(self):
         # GIVEN datapackage that can be treated as valid by the dpm
         self.valid_dp = datapackage.DataPackage({
-            "name": "some-datapackage",
-            "resources": [
-                {"name": "some-resource", "path": "./data/some_data.csv", }
-            ]
-        })
-        patch('dpm.main.client.do_publish.validate',
-              lambda *a: self.valid_dp).start()
+                "name": "some-datapackage",
+                "resources": [
+                    {"name": "some-resource", "path": "./data/some_data.csv", }
+                ]
+            },
+            default_base_path='.')
+        patch('dpm.client.DataPackage', lambda *a: self.valid_dp).start()
+        patch('dpm.client.exists', lambda *a: True).start()
 
-    @patch('dpm.client.do_publish.filter', lambda a, b: ['README.md'])
+    @patch('dpm.client.filter', lambda *a: ['README.md'])
     @patch('dpm.utils.file.open', mock_open())  # mock csv file open
     @patch('dpm.utils.file.getsize', lambda a: 5)  # mock csv file size
-    @patch('dpm.client.do_publish.md5_file_chunk', lambda a:
+    @patch('dpm.client.md5_file_chunk', lambda a:
            '855f938d67b52b5a7eb124320a21a139')  # mock md5 checksum
     def test_publish_success(self):
         # GIVEN the registry server that accepts any user
@@ -75,7 +66,7 @@ class PublishSuccessTest(BaseCliTestCase):
             status=200)
 
         # WHEN `dpm publish` is invoked
-        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
+        result = self.invoke(cli, ['publish'])
 
         # THEN 'publish ok' should be printed to stdout
         self.assertRegexpMatches(result.output, 'publish ok')
@@ -93,7 +84,7 @@ class PublishSuccessTest(BaseCliTestCase):
                 # POST authorize presigned url for s3 upload
                 ('POST', 'https://example.com/api/auth/bitstore_upload',
                     {"publisher": "user", "package": "some-datapackage",
-                     "path": "some_data.csv", "md5": '855f938d67b52b5a7eb124320a21a139'}),
+                     "path": "./data/some_data.csv", "md5": '855f938d67b52b5a7eb124320a21a139'}),
                 # PUT data to s3
                 ('PUT', 'https://s3.fake/put_here', ''),
                 # POST authorized presigned url for README
