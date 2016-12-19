76ebdd780b615a21ef1cb5311aadd54d2cfc031a
Roman Evstifeev <someuniquename@gmail.com>
2016-12-19 20:39:22 +0300

    Fix test_publish_invalid.py
    Addreses #55

--- tests/test_publish_invalid.py
+++ tests/test_publish_invalid.py
@@ -12,7 +12,7 @@ from mock import patch, MagicMock, Mock
 from click.testing import CliRunner, Result
 
 from dpm.main import cli
-from .base import BaseCliTestCase
+from .base import BaseCliTestCase, StringIO
 
 
 class PublishInvalidTest(BaseCliTestCase):
@@ -23,13 +23,16 @@ class PublishInvalidTest(BaseCliTestCase):
 
     def setUp(self):
         # GIVEN datapackage that can be treated as valid by the dpm
-        valid_dp = datapackage.DataPackage({
-            "name": "some-datapackage",
-            "resources": [
-                { "name": "some-resource", "path": "./data/some_data.csv", }
-            ]
-        })
-        patch('dpm.main.client.do_publish.validate', lambda *a: valid_dp).start()
+        self.valid_dp = datapackage.DataPackage({
+                "name": "some-datapackage",
+                "resources": [
+                    { "name": "some-resource", "path": "./data/some_data.csv", }
+                ]
+            },
+            default_base_path='.')
+        patch('dpm.client.DataPackage', lambda *a: self.valid_dp).start()
+        patch('dpm.client.exists', lambda *a: True).start()
+
 
     def test_publish_invalid(self):
         # GIVEN the server that accepts any user
@@ -44,84 +47,9 @@ class PublishInvalidTest(BaseCliTestCase):
                 status=400)
 
         # WHEN `dpm publish` is invoked
-        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
+        result = self.invoke(cli, ['publish'])
 
         # THEN 'datapackage.json is invalid' should be printed to stdout
         self.assertRegexpMatches(result.output, 'invalid datapackage json')
-        # AND exit code should be 1
-        self.assertEqual(result.exit_code, 1)
-
-    def test_missing_credentials(self):
-        #Testing Missing Credentials
-        self._config = ConfigObj({
-        'username': 'user',
-        })
-        self.config = MagicMock(spec_set=self._config)
-        self.config.__getitem__.side_effect = self._config.__getitem__
-        self.config.__setitem__.side_effect = self._config.__setitem__
-        self.config.get.side_effect = self._config.get
-        patch('dpm.main.ConfigObj', lambda *a: self.config).start()
-
-        self.runner = CliRunner()
-        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
-        self.assertRegexpMatches(result.output, 'missing user credentials')
-
-    def test_notify_token_not_received(self):
-        #Testing token received
-        # GIVEN the server that accepts any user
-        self.valid_dp = datapackage.DataPackage({
-            "name": "some-datapackage",
-            "resources": [
-                { "name": "some-resource", "path": "./data/some_data.csv", }
-            ]
-        })
-        patch('dpm.main.client.do_publish.validate', lambda *a: self.valid_dp).start()
-
-        responses.add(
-                responses.POST, 'https://example.com/api/auth/token',
-                json={"token":""},
-                status=200)
-        # WHEN `dpm publish` is invoked
-        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
-
-        # THEN 'datapackage.json is invalid' should be printed to stdout
-        self.assertRegexpMatches(result.output, 'server did not return auth token')
-        # AND exit code should be 1
-        self.assertEqual(result.exit_code, 1)
-
-    @patch('dpm.client.do_publish.md5_file_chunk',
-           lambda a: '855f938d67b52b5a7eb124320a21a139')  # mock md5 checksum
-    def test_notify_put_url_not_received(self):
-        #Testing notificaion on put url
-        # GIVEN the server that accepts any user
-        self.valid_dp = datapackage.DataPackage({
-            "name": "some-datapackage",
-            "resources": [
-                { "name": "some-resource", "path": "./data/some_data.csv", }
-            ]
-        })
-        patch('dpm.main.client.do_publish.validate', lambda *a: self.valid_dp).start()
-
-        responses.add(
-                responses.POST, 'https://example.com/api/auth/token',
-                json={"token":"sometoken"},
-                status=200)
-        # AND registry server accepts any datapackage
-        responses.add(
-                responses.PUT, 'https://example.com/api/package/user/some-datapackage',
-                json={'message': 'OK'},
-                status=200)
-        # AND registry server gives bitstore upload url
-        responses.add(
-                responses.POST, 'https://example.com/api/auth/bitstore_upload',
-                json={'key': ""},
-                status=200)
-        # AND s3 server allows data upload
-
-        # WHEN `dpm publish` is invoked
-        result = self.invoke(cli, ['publish', '--publisher', 'testpub'])
-
-        # THEN 'datapackage.json is invalid' should be printed to stdout
-        self.assertRegexpMatches(result.output, 'server did not return resource put url')
         # AND exit code should be 1
         self.assertEqual(result.exit_code, 1)
