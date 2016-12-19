9bd2bc31fe3d65512567825bff54879aa9f47e92
Roman Evstifeev <someuniquename@gmail.com>
2016-12-19 19:16:09 +0300

    Fix test_configure_success.py
    Addresses #55

--- tests/test_configure_success.py
+++ tests/test_configure_success.py
@@ -9,7 +9,7 @@ import responses
 from mock import patch
 
 from dpm.main import cli
-from .base import BaseCliTestCase
+from .base import BaseCliTestCase, StringIO
 
 
 class ConfigureSuccessTest(BaseCliTestCase):
@@ -18,20 +18,33 @@ class ConfigureSuccessTest(BaseCliTestCase):
     configuration should be saved to disk.
     """
 
+    def setUp(self):
+        # GIVEN datapackage that can be treated as valid by the dpm
+        self.valid_dp = datapackage.DataPackage({
+            "name": "some-datapackage",
+            "resources": [
+                {"name": "some-resource", "path": "./data/some_data.csv", }
+            ]
+        })
+        patch('dpm.client.DataPackage', lambda *a: self.valid_dp).start()
+        patch('dpm.client.os.path.exists', lambda *a: True).start()
+
     def test_configure_success(self):
         # GIVEN valid inputs for options
         options = {
             'Username: ': 'user',
-            'Your password: ': 'password',
+            'Your password (input hidden): ': 'password',
             'Server URL: ': 'http://example.com'
         }
-        patch('dpm.client.do_configure.input', lambda opt: options[opt]).start()
-        patch('dpm.client.do_configure.getpass', lambda opt: options[opt]).start()
+        patch('dpm.config.input', lambda opt: options[opt]).start()
+        patch('dpm.config.getpass', lambda opt: options[opt]).start()
 
         # WHEN `dpm configure` is invoked
         result = self.invoke(cli, ['configure'])
 
-        # THEN config should be written to disk
+        # THEN 'Configuration saved' should be printed to stdout
+        self.assertRegexpMatches(result.output, 'Configuration saved')
+        # AND config should be written to disk
         self.config.write.assert_called_once()
         # AND exit code should be 0
         self.assertEqual(result.exit_code, 0)
