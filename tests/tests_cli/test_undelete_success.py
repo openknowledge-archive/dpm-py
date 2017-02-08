6b294f345d61906c4b2c3ad0d9db6099a6541200
Roman Evstifeev <someuniquename@gmail.com>
2017-02-08 22:32:29 +0300

    Implement undelete command. Fixes #40

--- tests/tests_cli/test_undelete_success.py
+++ tests/tests_cli/test_undelete_success.py
@@ -15,9 +15,9 @@ from dpm.main import cli
 from ..base import BaseCliTestCase, jsonify
 
 
-class DeletePurgeSuccessTest(BaseCliTestCase):
+class UndeleteSuccessTest(BaseCliTestCase):
     """
-    When user deletes or purges datapackage, dpm should report sucess.
+    When user undeletes datapackage, dpm should report sucess.
     """
 
     def setUp(self):
@@ -37,20 +37,15 @@ class DeletePurgeSuccessTest(BaseCliTestCase):
             responses.POST, 'https://example.com/api/auth/token',
             json={'token': 'blabla'},
             status=200)
-        # AND registry server accepts deletion of any datapackage
+        # AND registry server accepts undeletion of any datapackage
         responses.add(
-            responses.DELETE, 'https://example.com/api/package/user/some-datapackage',
-            json={'message': 'OK'},
-            status=200)
-        # AND registry server accepts purging of any datapackage
-        responses.add(
-            responses.DELETE, 'https://example.com/api/package/user/some-datapackage/purge',
+            responses.POST, 'https://example.com/api/package/user/some-datapackage/undelete',
             json={'message': 'OK'},
             status=200)
 
-    def test_delete_success(self):
-        # WHEN `dpm delete` is invoked
-        result = self.invoke(cli, ['delete'])
+    def test_undelete_success(self):
+        # WHEN `dpm undelete` is invoked
+        result = self.invoke(cli, ['undelete'])
 
         # THEN 'delete ok' should be printed to stdout
         self.assertRegexpMatches(result.output, 'delete ok')
@@ -62,26 +57,7 @@ class DeletePurgeSuccessTest(BaseCliTestCase):
                 # POST authorization
                 ('POST', 'https://example.com/api/auth/token',
                     {"username": "user", "secret": "access_token"}),
-                # DELETE datapackage
-                ('DELETE', 'https://example.com/api/package/user/some-datapackage', '')])
-        # AND exit code should be 0
-        self.assertEqual(result.exit_code, 0)
-
-    def test_purge_success(self):
-        # WHEN `dpm purge` is invoked
-        result = self.invoke(cli, ['purge'])
-
-        # THEN 'purge ok' should be printed to stdout
-        self.assertRegexpMatches(result.output, 'purge ok')
-        # AND 2 requests should be sent
-        self.assertEqual(
-            [(x.request.method, x.request.url, jsonify(x.request))
-             for x in responses.calls],
-            [
-                # POST authorization
-                ('POST', 'https://example.com/api/auth/token',
-                    {"username": "user", "secret": "access_token"}),
-                # DELETE datapackage
-                ('DELETE', 'https://example.com/api/package/user/some-datapackage/purge', '')])
+                # POST undelete datapackage
+                ('POST', 'https://example.com/api/package/user/some-datapackage/undelete', '')])
         # AND exit code should be 0
         self.assertEqual(result.exit_code, 0)
